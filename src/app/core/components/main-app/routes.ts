7f6c5e6b59b964d8cdcd7e8b74fe0bbec5b9b683
GiorgiChalagashvili <chalagashvili.giorgi@gmail.com>
2023-10-19 23:59:22 +0400

    added logout, started working on main page

--- src/app/core/components/main-app/routes.ts
+++ src/app/core/components/main-app/routes.ts
@@ -9,12 +9,16 @@ export const ROUTES: Routes = [
         loadComponent: ()=>import('../../../features/registration/components/registration/registration.component').then(m => m.RegistrationComponent),
     },
     {
-        path: "",
+        path: "", 
         loadComponent: ()=>import('../starting-page/starting-page.component').then(m => m.StartingPageComponent),
     },
     {
         path: 'login',
         loadComponent: ()=>import('../../../features/login/components/login/login.component').then(m => m.LoginComponent),
+    },
+    {
+        path: 'homepage',
+        loadComponent: ()=> import('../../../features/home-page/components/home-page/home-page.component').then(m => m.HomePageComponent),
     },
    
 ];
--- src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AuthorizationService } from '../../services/authorization-services/authorization.service';
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
@@ -8,9 +10,30 @@ import { Router } from '@angular/router';
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss']
 })
-export class NavBarComponent {
+export class NavBarComponent{
 
-  constructor(private router: Router){};
+  constructor(private router: Router, private userControl: AuthorizationService){
+    userControl.currentState.subscribe(
+      (data) => {
+        this.loggedIn = (data.currentUserId == "-1" ? false : true);
+      }
+    );
+  };
+
+  public loggedIn: boolean = false;
+  private subscription: Subscription | undefined;
+
+  ngOnInit(): void {
+    this.subscription = this.userControl.currentState.subscribe((userState)=>{
+      userState.currentUserId === "-1" ? this.loggedIn = false : this.loggedIn = true;
+    })
+  }
+
+  
+  ngOnDestroy(): void {
+    (this.subscription as Subscription).unsubscribe();
+  }
+
 
   public redirectToLoginPage(): void{
     this.router.navigate(['/login']);
@@ -19,5 +42,18 @@ export class NavBarComponent {
 
   public redirectToRegisterPage(): void{
     this.router.navigate(['/register']);
+  }
+
+  public redirectToLandingPage(): void{
+    this.router.navigate(['/']);
+  }
+  public redirectToHomePage(): void{
+    this.router.navigate(['/homepage']);
+  }
+
+  public logOut(): void{
+    this.userControl.logOut();
+    this.userControl.currUserId = "-1";
+    this.redirectToLandingPage();
   }
 }
