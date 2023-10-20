15cff1146cc49aa2a6f5f038bf0cf43c244a8e48
GiorgiChalagashvili <chalagashvili.giorgi@gmail.com>
2023-10-21 01:39:42 +0400

    updated home page

--- src/app/features/home-page/components/home-page/home-page.component.ts
+++ src/app/features/home-page/components/home-page/home-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { customPassValidator } from 'src/app/shared/validators/validators';
+import { IUser, posts } from 'src/app/shared/interfaces/interfaces';
+import { AuthorizationService } from 'src/app/shared/services/authorization-services/authorization.service';
 @Component({
   selector: 'app-home-page',
   standalone: true,
@@ -10,17 +12,49 @@ import { customPassValidator } from 'src/app/shared/validators/validators';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent {
-  constructor(public formBuilder: FormBuilder){};
+  constructor(public formBuilder: FormBuilder, public userControl: AuthorizationService){};
 
   public detailedInfoForm = this.formBuilder.group({
-    levelSelect: ['', Validators.required],
+    academicState: ['', Validators.required],
     subjectSelect: ['', Validators.required],
     availabilitySelect: ['', Validators.required],
     locationSelect: ['', Validators.required],
     languageSelect: ['', Validators.required],
     priceSelect: ['', Validators.required],
-    description: ['', Validators.required],
+    description: [''],
   });
+
+  allPosts: posts[] = [];
+  loggedInId: string = "";
+  loggedInUser: IUser = {id: "-1", email: "", nickname: "", password: "", posts: []};
+  
+
+  ngOnInit(){
+    this.loggedInId = this.userControl.currUserId;
+    this.loggedInUser = this.userControl.getUser(this.loggedInId);
+    //posts = getAllPosts()
+  }
+
+  public postInfo(){
+    let newPost = this.generatePost();
+    console.log(newPost);
+    
+    
+  }
+
+  private generatePost(): string{
+    const data = this.detailedInfoForm.value;
+    let res = `Hello, my name is ${this.loggedInUser.nickname}, `;
+    console.log(data.academicState);
+    
+    if(data.academicState === 'teach') res += `I'm here to help you with ${data.subjectSelect}. `;
+    if(data.academicState === 'learn') res += `I'm here to find someone who can help me with ${data.subjectSelect}. `;
+    res += `My prefered time during the week is ${data.availabilitySelect}, as of the place of lessons, I would prefer ${data.locationSelect} and prefered language would be ${data.languageSelect}. For me acceptable price per lesson would be around ${data.priceSelect}. \n`
+    if(data.description !== "") res += `Some additional information about me: ${data.description}`;
+    return res;
+  }
+
+ 
 }
 
 
--- src/app/shared/interfaces/interfaces.ts
+++ src/app/shared/interfaces/interfaces.ts
@@ -13,3 +13,13 @@ export interface data{
     users: IUser[],
     currentUserId: string
 }
+
+export interface posts{
+    levelSelect: '',
+    subjectSelect: '',
+    availabilitySelect: '',
+    locationSelect: '',
+    languageSelect: '',
+    priceSelect: '',
+    description: '',
+}
