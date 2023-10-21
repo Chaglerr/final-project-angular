489539ef1884bf6ee7f853f7468aeff5360d1f09
GiorgiChalagashvili <chalagashvili.giorgi@gmail.com>
2023-10-21 14:13:36 +0400

    added validators on homepage

--- src/app/features/home-page/components/home-page/home-page.component.ts
+++ src/app/features/home-page/components/home-page/home-page.component.ts
@@ -35,20 +35,36 @@ export class HomePageComponent {
     //posts = getAllPosts()
   }
 
+
+  defaultFormValues = {
+    academicState: '', 
+    subjectSelect: '',
+    availabilitySelect: '',
+    locationSelect: '',
+    languageSelect: '',
+    priceSelect: '',
+    description: '',
+  };
+
   public postInfo(){
     let newPost = this.generatePost();
+    if(newPost === "") return;
     this.loggedInUser.posts.push(newPost);
     this.allPosts.push(newPost);
-    console.log(newPost);
+  
+    this.detailedInfoForm.reset(this.defaultFormValues);
+    //console.log(newPost);
   }
 
   private generatePost(): string{
     const data = this.detailedInfoForm.value;
     let res = `Hello, my name is ${this.loggedInUser.nickname}, `;
-    console.log(data.academicState);
-    
     if(data.academicState === 'teach') res += `I'm here to help you with ${data.subjectSelect}. `;
-    if(data.academicState === 'learn') res += `I'm here to find someone who can help me with ${data.subjectSelect}. `;
+    else if(data.academicState === 'learn') res += `I'm here to find someone who can help me with ${data.subjectSelect}. `;
+    else return "";
+    if(!data.availabilitySelect || !data.priceSelect || !data.subjectSelect || !data.locationSelect || !data.languageSelect) return "";
+    
+    if(data.availabilitySelect === 'Select Availability') return "";
     res += `My prefered time during the week is ${data.availabilitySelect}, as of the place of lessons, I would prefer ${data.locationSelect} and prefered language would be ${data.languageSelect}. For me acceptable price per lesson would be around ${data.priceSelect}. \n`
     if(data.description !== "") res += `Some additional information about me: ${data.description}`;
     return res;
