19bda7416447cc298b0e9b67d856f4d501adcb8d
GiorgiChalagashvili <chalagashvili.giorgi@gmail.com>
2023-10-25 01:28:47 +0400

    fixed minimal bugs

--- src/app/core/components/main-app/routes.ts
+++ src/app/core/components/main-app/routes.ts
@@ -7,7 +7,7 @@ import { loggedIn, loggedOut } from "src/app/shared/guards/auth.guard";
 export const ROUTES: Routes = [
     {
         path: 'register',
-        //canActivate: [loggedOut],
+        canActivate: [loggedOut],
         loadComponent: ()=>import('../../../features/registration/components/registration/registration.component').then(m => m.RegistrationComponent),
     },
     {
@@ -16,22 +16,22 @@ export const ROUTES: Routes = [
     },
     {
         path: 'login',
-        //canActivate: [loggedOut],
+        canActivate: [loggedOut],
         loadComponent: ()=>import('../../../features/login/components/login/login.component').then(m => m.LoginComponent),
     },
     {
         path: 'homepage',
-        //canActivate: [loggedIn],
+        canActivate: [loggedIn],
         loadComponent: ()=> import('../../../features/home-page/components/home-page/home-page.component').then(m => m.HomePageComponent),
     },
     {
         path: 'myprofile',
-        //canActivate: [loggedIn],
+        canActivate: [loggedIn],
         loadComponent: ()=> import('../../../features/my-profile/components/my-profile/my-profile.component').then(m => m.MyProfileComponent),
     },
     {
         path: 'adminpanel',
-        //canActivate: [loggedIn],
+        canActivate: [loggedIn],
         loadComponent: ()=> import('../../../features/admin-panel/components/admin-panel/admin-panel.component').then(m => m.AdminPanelComponent),
     },
     {
